3cd9fa3f68265ec05617e510cddf265aa7186308
Abhishek Kumar <anabhishek674@gmail.com>
2026-02-22 11:58:46 +0530

    Add robot_news_station node and update package configuration

---
+++ src/my_py_pkg/my_py_pkg/robot_news_station.py
@@ -0,0 +1,29 @@
+import rclpy
+from rclpy.node import Node
+from example_interfaces.msg import String
+
+
+class robot_News_Station_Node (Node):
+        def __init__(self):
+            super().__init__("robot_news_station")
+            self.publisher_ = self.create_publisher(String, "robot_news", 10)
+            self.timer_ = self.create_timer(0.5, self.publish_news)
+            self.get_logger().info("Robot news station has been started")
+
+        def publish_news(self):
+             msg = String()
+             msg.data = "Namaste!"
+             self.publisher_.publish(msg)
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = robot_News_Station_Node()
+    rclpy.spin(node)()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
--- src/my_py_pkg/setup.py
+++ src/my_py_pkg/setup.py
@@ -24,7 +24,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'py_node = my_py_pkg.hello_node:main',
+            "py_node = my_py_pkg.hello_node:main",
+            "robot_news_station = my_py_pkg.robot_news_station:main"
         ],
     },
 
